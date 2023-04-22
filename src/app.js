dae60d5618b123ba2946f43df9dad183e7aa1ad9
Ryan Wibawa <ryan.wibawa@gmail.com>
2023-04-22 00:06:25 -0700

    draw the clock base only once

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { drawClock } from "./lib/clock.js";
+import { drawClockBase, drawClock } from "./lib/clock.js";
 
 // Create a canvas object (canvas) from the HTML canvas element:
 const canvas = document.getElementById("canvas-clock");
@@ -14,6 +14,9 @@ ctx.translate(radius, radius);
 
 // Reduce the clock radius (to 90%) to draw the clock well inside the canvas:
 radius = radius * 0.90;
+
+drawClockBase(ctx, radius);
+// drawClock(ctx, radius);
 
 // To start the clock, call the drawClock function at intervals:
 // The interval is in milliseconds. drawClock will be called for each 1000 milliseconds.
--- src/lib/clock.js
+++ src/lib/clock.js
@@ -1,3 +1,25 @@
+export function drawClockBase(ctx, radius) {
+  // Draw the clock brim:
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+
+  // Create a radial gradient (95% and 105% of original clock radius):
+  const grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+
+  // Create 3 color stops, corresponding with the inner, middle, and outer edge of the arc:
+  // The color stops create a 3D effect.
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+
+  // Define the gradient as the stroke style of the drawing object:
+  ctx.strokeStyle = grad;
+  // Define the line width of the drawing object (10% of radius):
+  ctx.lineWidth = radius*0.1;
+  // Draw the circle:
+  ctx.stroke();
+}
+
 export function drawClock(ctx, radius) {
   drawFace(ctx, radius);
   drawNumbers(ctx, radius);
@@ -5,19 +27,13 @@ export function drawClock(ctx, radius) {
 }
 
 function drawFace(ctx, radius) {
+  // Draw the white circle:
   ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+  ctx.arc(0, 0, radius * 0.95, 0, 2 * Math.PI);
   ctx.fillStyle = 'white';
   ctx.fill();
 
-  const grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
-  grad.addColorStop(0, '#333');
-  grad.addColorStop(0.5, 'white');
-  grad.addColorStop(1, '#333');
-  ctx.strokeStyle = grad;
-  ctx.lineWidth = radius*0.1;
-  ctx.stroke();
-
+  // Draw the clock center:
   ctx.beginPath();
   ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
   ctx.fillStyle = '#333';
