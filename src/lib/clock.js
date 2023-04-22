b8d2bc6eef9ef8b6bbf6468a34dc956babdced28
Ryan Wibawa <ryan.wibawa@gmail.com>
2023-04-21 22:34:32 -0700

    add clock face

--- src/lib/clock.js
+++ src/lib/clock.js
@@ -1,8 +1,29 @@
 export function drawClock(ctx, radius) {
+  // Remap the (0,0) position (of the drawing object) to the center of the canvas:
   ctx.translate(radius, radius);
+
+  // Remap the (0,0) position (of the drawing object) to the center of the canvas:
   radius = radius * 0.90
 
-  ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
-  ctx.fillStyle = "white";
+  drawFace(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  const grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+  ctx.fillStyle = '#333';
   ctx.fill();
 }
